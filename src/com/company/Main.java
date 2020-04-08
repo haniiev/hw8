49aee23571d98f62f31325df7f93a849a56462e0
Anton <39731038+haniiev@users.noreply.github.com>
2020-04-09 01:28:18 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,33 @@
 package com.company;
 
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String aaa = "IT Academy";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("surname:");
+        String surname = reader.readLine();
+        System.out.print("name:");
+        String name = reader.readLine();
+        System.out.print("patronymic:");
+        String patronymic = reader.readLine();
 
-        if(aaa.contains("IT")){
-          System.out.println("true");
-        }
-        else {
-            System.out.println("false");
-        }
+        String format1 = surname +" "+ "%.1s"+"%.1s";
+        System.out.println(String.format(format1,name,patronymic));
+
+        System.out.println(name);
+
+        StringBuilder fa = new StringBuilder();
+        fa.append(name);
+        fa.append(", ");
+        fa.append(patronymic);
+        fa.append(" ");
+        fa.append(surname);
+        System.out.println(fa);
+
+
     }
 }
