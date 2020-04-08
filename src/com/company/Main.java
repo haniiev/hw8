8000914d9617abb6fb83654fcba170912d43084f
Anton <39731038+haniiev@users.noreply.github.com>
2020-04-09 01:31:34 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,29 @@
 package com.company;
 
-import javax.swing.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        String aaa = "IT Academy";
+        String a1 = "jkDS@jn_545";
+        String a2 = "hjLb_15kjn1";
+        String a3 = "j";
+        String a4 = "hLjb_1";
+        String a5 = "hhgOhbb_151";
 
-        if(aaa.contains("IT")){
-          System.out.println("true");
-        }
-        else {
-            System.out.println("false");
-        }
+        Pattern b = Pattern.compile("^[a-z0-9A-Z_]{3,15}$" );
+        Matcher c1 = b.matcher(a1);
+        Matcher c2 = b.matcher(a2);
+        Matcher c3 = b.matcher(a3);
+        Matcher c4 = b.matcher(a4);
+        Matcher c5 = b.matcher(a5);
+
+        System.out.println(c1.matches());
+        System.out.println(c2.matches());
+        System.out.println(c3.matches());
+        System.out.println(c4.matches());
+        System.out.println(c5.matches());
+
     }
 }
