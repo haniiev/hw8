dfdc936569bb3aaf23b0c34e46cae0e1ffc0c5b5
Anton Haniyev <anton199906@gmail.com>
2020-04-09 01:39:04 +0300

    c1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String aaa = "IT Academy";
+
+        if(aaa.contains("IT")){
+          System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
